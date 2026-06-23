1d1d314286c15e06e8f3c1664561a3a8b8692954
MambasWolf <138817150+MambasWolf@users.noreply.github.com>
2026-06-23 20:38:59 +0200

    Update build.gradle.kts

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,14 +1,6 @@
 group = "app.mambaswolf.patches"
 
-about {
-    name = "Piti messages tout doux"
-    description = "Affiche un message quand on clique sur l'onglet Reels"
-    source = "https://github.com/MambasWolf/Piti-Messages-tout-doux"
-    author = "MambasWolf"
-    contact = ""
-    website = "https://github.com/MambasWolf/Piti-Messages-tout-doux"
-    license = "GNU General Public License v3.0"
-}
+
 
 patches {
     about {
